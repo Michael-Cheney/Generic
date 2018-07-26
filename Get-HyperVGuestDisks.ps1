3a217e5200537ba717199d2d49e5c12e3050fb0d
Michael Cheney <31533370+Michael-Cheney@users.noreply.github.com>
2018-07-26 13:27:45 +1000

    Add files via upload

---
+++ Get-HyperVGuestDisks.ps1
@@ -0,0 +1,21 @@
+﻿# Get a list of all Cluster Nodes
+Begin {
+    $ClusterNodes = Get-ClusterNode
+    $Disks = @()
+    $VM = ""
+}
+
+Process {
+    foreach ($Node in $ClusterNodes) {
+        $VM = Get-VM -ComputerName $Node
+        foreach ($V in $VM) {
+        $Disks += Get-VMHardDiskDrive $V
+        }
+    }
+}
+
+End {
+$Disks | ConvertTo-Csv | Out-File C:\temp\guestvhdlist.csv
+}
+
+
